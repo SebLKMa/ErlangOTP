0f4264e6f3e7c821031c1fe50cf5495dd961a5ba
user <user@192.168.0.104>
2016-07-11 22:26:56 +0800

    added sc_sup

---
+++ simple_cache/src/sc_sup.erl
@@ -0,0 +1,33 @@
+%% @author user
+%% @doc @todo Add description to sc_sup.
+
+
+-module(sc_sup).
+
+-behaviour(supervisor).
+
+%% ====================================================================
+%% API functions
+%% ====================================================================
+-export([start_link/0, start_child/2, init/1]).
+
+-define(SERVER, ?MODULE).
+
+start_link() ->
+	supervisor:start_link({local, ?SERVER}, ?MODULE, []).
+
+start_child(Value, LeaseTime) ->
+	supervisor:start_child(?SERVER, [Value, LeaseTime]).
+
+init([]) ->
+	Element = {sc_element, {sc_element, start_link, []},
+			   temporary, brutal_kill, worker, [sc_element]},
+	Children = [Element],
+	RestartStrategy = {simple_one_for_one, 0, 1},
+	{ok, {RestartStrategy, Children}}.
+
+%% ====================================================================
+%% Internal functions
+%% ====================================================================
+
+
