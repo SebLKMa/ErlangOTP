a36953ff99e6712311853949b09f6911a34691cb
seblkma <sebmalikkeung@gmail.com>
2016-07-12 16:51:47 +0800

    updated sc_app

--- simple_cache/src/sc_app.erl
+++ simple_cache/src/sc_app.erl
@@ -11,10 +11,10 @@
 -export([start/2, stop/1]).
 
 start(_StartType, _StartArgs) ->
+	sc_store:init(),
 	case sc_sup:start_link() of
 		{ok, Pid} -> {ok, Pid};
-	Error ->
-		{Error}
+		Other -> {error, Other}
 	end.
 
 stop(_State) ->
