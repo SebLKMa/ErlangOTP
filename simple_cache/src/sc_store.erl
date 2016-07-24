4367cac871dbee85afa4b90cdb75af2bc692537b
user <user@192.168.0.104>
2016-07-24 15:48:14 +0800

    mnesia impl

--- simple_cache/src/sc_store.erl
+++ simple_cache/src/sc_store.erl
@@ -14,24 +14,69 @@
 
 -define(TABLE_ID, ?MODULE).
 
+%% mnesia record
+-record(key_to_pid, {key, pid}).
+
+%% ets impl
+%init() ->
+%	ets:new(?TABLE_ID, [public, named_table]),
+%	ok.
+
+%% mnesia impl
+%% erl must be started with mnesia path, schema must have already created
+%% '"/Users/user/Documents/workspace/mars/github/ErlangOTP/mnesia_storage"'
 init() ->
-	ets:new(?TABLE_ID, [public, named_table]),
-	ok.
+	mnesia:start(),
+	mnesia:create_table(key_to_pid,		% table name to create
+						[{index, pid},	% index to create
+						 {attributes, record_info(fields, key_to_pid)}]).
 
+%% ets impl
+%insert(Key, Pid) ->
+%	ets:insert(?TABLE_ID, {Key, Pid}).
+
+%% mnesia impl
 insert(Key, Pid) ->
-	ets:insert(?TABLE_ID, {Key, Pid}).
+	mnesia:dirty_write(#key_to_pid{key = Key, pid = Pid}).
 
+%% ets impl
+%lookup(Key) ->
+%	case ets:lookup(?TABLE_ID, Key) of
+%		[{Key, Pid}] -> {ok, Pid};
+%		[] -> {error, not_found}
+%	end.
+
+%% mnesia impl
 lookup(Key) ->
-	case ets:lookup(?TABLE_ID, Key) of
-		[{Key, Pid}] -> {ok, Pid};
-		[] -> {error, not_found}
+	case mnesia:dirty_read(key_to_pid, Key) of
+		[{key_to_pid, Key, Pid}] -> 
+			case is_pid_alive(Pid) of 
+				true -> {ok, Pid};
+				false -> {error, not_found}
+			end;
+		[] -> 
+			{error, not_found}
 	end.
 
-delete(Pid) ->
-	ets:match_delete(?TABLE_ID, {'_', Pid}).
+%% ets impl
+%delete(Pid) ->
+%	ets:match_delete(?TABLE_ID, {'_', Pid}).
 
+% mnesia impl
+delete(Pid) ->
+	case mnesia:dirty_index_read(key_to_pid, Pid, #key_to_pid.pid) of
+		[#key_to_pid{} = Record] ->
+			mnesia:dirty_delete_object(Record);
+		_ ->
+			ok
+	end.
 
 %% ====================================================================
 %% Internal functions
 %% ====================================================================
+is_pid_alive(Pid) when node(Pid) =:= node() ->
+	is_process_alive(Pid); % see http://erlang.org/doc/man/erlang.html#is_process_alive-1
+is_pid_alive(Pid) ->
+	lists:member(node(Pid), nodes()) andalso
+		(rpc:call(node(Pid), erlang, is_process_alive, [Pid]) =:= true).
 
