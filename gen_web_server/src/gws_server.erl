8e9cd85be7dc4809a30e3af2332a3f83d121aaf0
user <user@192.168.0.104>
2016-08-31 23:14:34 +0800

    updated gws_server

--- gen_web_server/src/gws_server.erl
+++ gen_web_server/src/gws_server.erl
@@ -41,9 +41,86 @@ handle_call(_Request, _From, State) ->
 handle_cast(_Request, State) ->
 	{noreply, State}.
 
+handle_info({http, _Sock, {http_request, _, _, _} = Request}, State) ->
+	% handles request lines
+	inet:setopts(State#state.socket, [{active, once}]),
+	{noreply, State#state{request_line = Request}};
+handle_info({http, _Sock, {http_request, _, Name, _, Value}}, State) ->
+	% handles headers
+	inet:setopts(State#state.socket, [{active, once}]),
+	{noreply, header(Name, Value, State)};
+handle_info({http, _Sock, http_eoh}, #state{content_remaining = 0} = State) ->
+	% handles end of header, body is empty
+	{stop, normal, handle_http_request(State)};
+handle_info({http, _Sock, http_eoh}, State) ->
+	% handles end of header, prepare for body
+	inet:setopts(State#state.socket, [{active, once}, {packet, raw}]),
+	{noreply, State};
+handle_info({tcp, _Sock, Data}, State) when is_binary(Data) ->
+	ContentRem = State#state.content_remaining - byte_size(Data),
+	Body = list_to_binary([State#state.body, Data]),
+	NewState = State#state{body = Body,
+						   content_remaining = ContentRem},
+	if	(ContentRem > 0) ->
+			inet:setopts(State#state.socket, [{active, once}]),
+			{noreply, NewState};
+		true ->
+			{stop, normal, handle_http_request(NewState)}
+	end;
+handle_info({tcp_closed, _Sock}, State) ->
+	{stop, normal, State};
+handle_info(timeout, #state{lsock = LSock, parent = Parent} = State) ->
+	% waits for connection and starts new handler
+	{ok, Socket} = gen_tcp:accept(LSock),
+	gws_connection_sup:start_child(Parent),
+	inet:setopts(Socket, [{active, once}]),
+	{noreply, State#state{socket = Socket}}.
 
+terminate(_Reason, _State) ->
+	ok.
+
+code_change(_OldVsn, State, _Extra) ->
+	{ok, State}.
+
+	
 %% ====================================================================
 %% Internal functions
 %% ====================================================================
+header('Content-Length' = Name, Value, State) ->
+	% remember content length for later
+	ContentLength = list_to_integer(binary_to_list(Value)),
+	State#state{content_remaining = ContentLength,
+				headers = [{Name, Value} | State#state.headers]};
+header(<<"Expect">> = Name, <<"100-continue">> = Value, State) ->
+	gen_tcp:send(State#state.socket, gen_web_server:http_reply(100)),
+	State#state{headers = [{Name, Value} | State#state.headers]};
+header(Name, Value, State) ->
+	State#state{headers = [{Name, Value} | State#state.headers]}.
 
+handle_http_request(#state{callback = Callback,
+						   request_line = Request,
+						   headers = Headers,
+						   body = Body,
+						   user_data = UserData} = State) ->
+	% performs callback and get results
+	{http_request, Method, _, _} = Request,
+	Reply = dispatch(Method, Request, Headers, Body, Callback, UserData),
+	gen_tcp:send(State#state.socket, Reply),
+	State.
 
+dispatch('GET', Request, Headers, _Body, Callback, UserData) ->
+	Callback:get(Request, Headers, UserData);
+dispatch('DELETE', Request, Headers, _Body, Callback, UserData) ->
+	Callback:delete(Request, Headers, UserData);
+dispatch('HEAD', Request, Headers, _Body, Callback, UserData) ->
+	Callback:head(Request, Headers, UserData);
+dispatch('POST', Request, Headers, Body, Callback, UserData) ->
+	Callback:post(Request, Headers, Body, UserData);
+dispatch('PUT', Request, Headers, Body, Callback, UserData) ->
+	Callback:put(Request, Headers, Body, UserData);
+dispatch('TRACE', Request, Headers, Body, Callback, UserData) ->
+	Callback:trace(Request, Headers, Body, UserData);
+dispatch('OPTIONS', Request, Headers, Body, Callback, UserData) ->
+	Callback:options(Request, Headers, Body, UserData);
+dispatch(_Other, Request, Headers, Body, Callback, UserData) ->
+	Callback:other_methods(Request, Headers, Body, UserData).
